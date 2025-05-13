efc408829ad53b0e12699b9d7af3b4c2efc01d38
Hidayet <hidayet.achakzai@gmail.com>
2025-05-13 14:11:52 +0500

    fix

--- resources/views/layouts/schoolDashboard.blade.php
+++ resources/views/layouts/schoolDashboard.blade.php
@@ -374,7 +374,7 @@
             
             <ul class="sidebar-menu">
                 <li>
-                    <a href="{{ route('dashboard') }}" class="{{ request()->routeIs('dashboard') ? 'active' : '' }}">
+                    <a href="{{ route('dashboard') }}" class="{{ request()->routeIs('school.dashboard') ? 'active' : '' }}">
                         <i class="fas fa-tachometer-alt"></i> Dashboard
                     </a>
                 </li>
